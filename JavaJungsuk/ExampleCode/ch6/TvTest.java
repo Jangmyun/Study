f8b61f951428202d9bb6679665de1083ada803e8
Jangmyun <cndlsrb2739@gmail.com>
2023-08-14 20:57:13 +0900

    객체지향프로그래밍 공부내용 정리 (객체의 생성과 사용)

---
+++ JavaJungsuk/ExampleCode/ch6/TvTest.java
@@ -0,0 +1,31 @@
+package ExampleCode.ch6;
+class Tv {
+    //Tv의 멤버변수 (속성)
+    String color;
+    boolean power;
+    int channel;
+
+    //Tv의 메서드 (기능)
+    void power(){
+        power = !power;
+    }
+    void channelUp(){
+        channel++;
+    }
+    void channelDown(){
+        channel--;
+    }
+}
+public class TvTest {
+    public static void main(String[] args) {
+        Tv t ;      //Tv클래스의 참조변수 t를 선언 
+        t = new Tv();   //new 연산자로 Tv인스턴스 생성후 그 주소를 참조변수 t에 저장
+        //이러면 이제 참조변수 t를 이용해서 Tv 객체에 접근가능함.
+
+        t.channel = 7;                  //channel 멤버변수에 7 저장
+        t.channelDown();                //channel을 1 줄이는 메서드
+        System.out.println(t.channel);  //t.channel 결과 : 6
+
+        
+    }
+}
