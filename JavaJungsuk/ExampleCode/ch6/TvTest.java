0f041253fa600dfde9de99a7ddd477276a816a18
Jangmyun <cndlsrb2739@gmail.com>
2023-08-15 11:22:56 +0900

    객체지향프로그래밍 공부내용 예제코드 업데이트

--- JavaJungsuk/ExampleCode/ch6/TvTest.java
+++ JavaJungsuk/ExampleCode/ch6/TvTest.java
@@ -18,14 +18,19 @@ class Tv {
 }
 public class TvTest {
     public static void main(String[] args) {
-        Tv t ;      //Tv클래스의 참조변수 t를 선언 
-        t = new Tv();   //new 연산자로 Tv인스턴스 생성후 그 주소를 참조변수 t에 저장
+        Tv t1 ;      //Tv클래스의 참조변수 t를 선언 
+        t1 = new Tv();   //new 연산자로 Tv인스턴스 생성후 그 주소를 참조변수 t에 저장
         //이러면 이제 참조변수 t를 이용해서 Tv 객체에 접근가능함.
+        Tv t2;
+        t2 = new Tv();
+        
 
-        t.channel = 7;                  //channel 멤버변수에 7 저장
-        t.channelDown();                //channel을 1 줄이는 메서드
-        System.out.println(t.channel);  //t.channel 결과 : 6
-
+        t1.channel = 7;                  //channel 멤버변수에 7 저장
+        t1.channelDown();                //channel을 1 줄이는 메서드
+        System.out.println(t1.channel);  //t.channel 결과 : 6
+        System.out.println(t2.channel);
+        t2 = t1;
+        System.out.println(t2.channel);
         
     }
 }
