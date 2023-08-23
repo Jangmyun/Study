6c6321609252004fafbce9e5b51ceb3902dfceb2
Jangmyun <cndlsrb2739@gmail.com>
2023-08-24 00:43:23 +0900

    클래스 상속 예제코드 추가

---
+++ JavaJungsuk/ExampleCode/ch7/InheritanceEx1.java
@@ -0,0 +1,31 @@
+package ExampleCode.ch7;
+
+class Tv {
+    boolean power;
+    int channel;
+
+    void power(){ power = !power;}
+    void channelUp() { channel++;}
+    void channelDown() { channel--;}
+}
+
+class CaptionTv extends Tv {        // Tv 를 상속받는 CaptionTv 클래스 정의
+    boolean caption;
+    void displayCaption(String text){
+        if (caption) {
+            System.out.println(text);
+        }
+    }
+}
+
+public class InheritanceEx1 {
+    public static void main(String[] args) {
+        CaptionTv ctv = new CaptionTv();
+        ctv.channel = 10;
+        ctv.channelUp();
+        System.out.println(ctv.channel);
+        ctv.displayCaption("Hello World!");
+        ctv.caption = true;
+        ctv.displayCaption("Hello World!!");
+    }
+}
