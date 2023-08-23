85774e7577e9ad6b6871be352b15144436c31e25
Jangmyun <cndlsrb2739@gmail.com>
2023-08-24 01:18:17 +0900

    클래스의 포함관계를 이용하여 클래스 재사용

---
+++ JavaJungsuk/ExampleCode/ch7/InheritanceEx3.java
@@ -0,0 +1,17 @@
+package ExampleCode.ch7;
+
+class Point {
+    int x;  // x좌표
+    int y;  // y좌표
+}
+
+class Circle {
+    Point point = new Point();
+    int r;      // 반지름
+}
+
+public class InheritanceEx3 {
+    public static void main(String[] args) {
+        
+    }    
+}
