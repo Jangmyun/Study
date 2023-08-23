4f9358172e26c50a68804103fa712f83732561e0
Jangmyun <cndlsrb2739@gmail.com>
2023-08-24 01:09:37 +0900

    부모 클래스 변경시 자식 클래스 영향 예제코드 보충

--- JavaJungsuk/ExampleCode/ch7/InheritanceEx2.java
+++ JavaJungsuk/ExampleCode/ch7/InheritanceEx2.java
@@ -9,6 +9,14 @@ class Child extends Parent {    // 자식 클래스의 멤버 2개 (부모로부
     }
 }
 
+class Point {   // 2차원 좌표의 한 점 클래스
+    int x;
+    int y;
+}
+class Point3D extends Point {   //  3차원 좌표의 한점 클래스 
+    int z;                      //  Point 클래스에서 상속받은 x,y 좌표에 z좌표만 추가해서 Point3D 클래스 생성
+}
+
 public class InheritanceEx2 {
     public static void main(String[] args) {
         
