0aa2e3293d13a6f90b742541e5e978c1f1eb6b1b
Jangmyun <cndlsrb2739@gmail.com>
2023-08-24 01:26:18 +0900

    코드에 대한 주석 설명 추가

--- JavaJungsuk/ExampleCode/ch7/InheritanceEx3.java
+++ JavaJungsuk/ExampleCode/ch7/InheritanceEx3.java
@@ -6,7 +6,7 @@ class Point {
 }
 
 class Circle {
-    Point point = new Point();
+    Point point = new Point();      // 다른 클래스 타입의 참조변수 생성 => 포함관계를 이용하여 단위별로 클래스를 분할 작성 => 코드의 이해를 돕는다.
     int r;      // 반지름
 }
 
