6433cbc0a5c86ccc9f7b091b323887bddff099c3
Jangmyun <cndlsrb2739@gmail.com>
2023-08-24 01:40:56 +0900

    6장에서 배운내용 (생성자) 을 사용해서 예제코드 보충

--- JavaJungsuk/ExampleCode/ch7/InheritanceEx3.java
+++ JavaJungsuk/ExampleCode/ch7/InheritanceEx3.java
@@ -10,8 +10,31 @@ class Circle {
     int r;      // 반지름
 }
 
+
+//다른 예시
+class Engine {   //  Car 클래스에 들어갈 Engine 클래스
+    int cylinder;       //  Engine 객체의 인스턴스 변수 설정 (몇기통인지, 마력은 몇인지)
+    int hoursePower;
+    
+    Engine(int cylinder, int hoursePower) {
+        this.cylinder = cylinder;
+        this.hoursePower = hoursePower;
+        System.out.printf("Your Car Engine has %d cylinder and it's hoursepower is %d%n" , cylinder,hoursePower);       // Engine 인스턴스 생성시 출력될 문장
+    }
+
+    Engine() {
+        this(4, 140);       // 기본 생성자에 this메서드로 파라미터 집어넣기
+    }
+}    
+class Door {    //  Car 클래스에 들어갈 Door 클래스
+    int number;
+}       
+class Car {                         
+    Engine engine = new Engine();       // Car 클래스 안에 Engine과 Door 클래스 타입의 참조변수를 인스턴스변수로 생성
+    Door door = new Door();
+}
 public class InheritanceEx3 {
     public static void main(String[] args) {
-        
+        Car c = new Car();
     }    
 }
