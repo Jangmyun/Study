f02fb710b4755c81a8fa891c29c5841ed80da567
Jangmyun <cndlsrb2739@gmail.com>
2023-08-24 11:16:42 +0900

    클래스의 카드 rank 변수를 JQK와 숫자로 나눈 것을 하나로 통합

--- JavaJungsuk/ExampleCode/ch7/CompositeEx1.java
+++ JavaJungsuk/ExampleCode/ch7/CompositeEx1.java
@@ -2,21 +2,22 @@ package ExampleCode.ch7;
 
 class Card {
     String kind;
-    char major;
-    int number;
+    String rank;
 
     static String[] kindArr = { "Spade" , "Heart", "Diamond", "Clover"};
-    static char[] majorArr = { 'K', 'Q', 'J'};
-    static int[] numberArr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    static String[] rankArr = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
-    Card(String kind, char major, int number) {
+    Card(String kind, String rank) {
         this.kind = kind;
-        this.major = major;
-        this.number = number;
+        this.rank = rank;
     }
 }
 class Deck {
     Card[] deck = new Card[52];     //트럼프 카드 총 52장 이므로 길이가 52인 객체배열 생성
+
+    for (String kind: Card.kindArr ) {
+        
+    } // end of kind
 }
 
 public class CompositeEx1 {
