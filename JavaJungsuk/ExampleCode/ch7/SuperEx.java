323c42987dc7f965fe35fa587f57f7e1dfede332
Jangmyun <cndlsrb2739@gmail.com>
2023-08-26 19:25:38 +0900

    들여쓰기 수정

--- JavaJungsuk/ExampleCode/ch7/SuperEx.java
+++ JavaJungsuk/ExampleCode/ch7/SuperEx.java
@@ -21,9 +21,10 @@ class SuperPoint3D extends SuperPoint {
         //생성자 첫줄에 자신 또는 부모 생성자 호출해야함 `this` or `super`
         // 부모 클래스인 SuperPoint 클래스에서 기본 생성자가 정의되어 있으므로 에러가 뜨지 않지만,
         // 기본 생성자 없고 SuperPoint3D 클래스에 부모 생성자 호출하지 않아서 부모 클래스의 기본 생성자 super()가 자식 클래스 생성자 첫줄에 들어가서 오류날거임
-        this.x=x;
-        this.y=y;
-        this.z=z;
+        // this.x=x;
+        // this.y=y;
+        super(x, y);        // SuperPoint(int x, int y) 호출
+        this.z=z;           
     }
 }
 
