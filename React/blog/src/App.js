3112d52995ed9314f35f234606ffcacd8e124d02
Jangmyun <cndlsrb2739@gmail.com>
2023-08-28 18:34:24 +0900

    블로그 글 리스트 생성

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -1,17 +1,18 @@
-
 import './App.css';
+import { useState } from "react"; //useState 사용할때 import 해야함
 
 function App() {
 
   let blogTitle = "New Blog";
-
+  var [articleTitle, b] = useState('블로그 첫 글'); // JS의 Destructuring 문법 사용한 useState생성
+  
   return (
     <div className="App">
       <div class="nav">
-        <h4 style={{fontSize :'30px'}}>{blogTitle}</h4>   
+        <h4 style={{fontSize :'30px'}}>{ blogTitle }</h4>
       </div>
       <div className="list">
-        <h4>Articl Title</h4>
+        <h4>{ articleTitle }</h4>
         <p>2023.07.57</p>
       </div>
     </div>
