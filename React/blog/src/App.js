fd200950fb92ea8d150d9338045e583750d631a1
Jangmyun <cndlsrb2739@gmail.com>
2023-08-30 18:56:59 +0900

    props를 이용해서 모달창 글 수정 버튼만들기

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useState } from "react"; //useState 사용할때 import 해야함
 
 function App() {
-  let [articleTitle] = useState(['블로그 첫 글', '블로그 두번째 글', 'JS 독학']); // JS의 Destructuring 문법 사용한 useState생성
+  let [articleTitle, setArticleTitle] = useState(['블로그 첫 글', '블로그 두번째 글', 'JS 독학']); // JS의 Destructuring 문법 사용한 useState생성
   
   let likeArr = articleTitle.map(function(){  // 글 개수만큼 0으로 채워진 배열 생성
     return 0;
@@ -20,7 +20,9 @@ function App() {
         articleTitle.map(function(value, index){
           return (
             <div className="list" key={index}>
-              <h4>{ articleTitle[index] }   <span onClick={(e)=>{
+              <h4 onClick={()=>{
+                setModal(!modal);
+              }}>{ articleTitle[index] }   <span onClick={()=>{
                 let copy = [...like];
                 copy[index] = copy[index]+1;
                 setLike(copy);
@@ -36,7 +38,7 @@ function App() {
       }}>modal</button>
       
       {
-        modal ? <Modal></Modal> : null
+        modal ? <Modal articleTitle={articleTitle} setArticleTitle={setArticleTitle}></Modal> : null
       }
     </div>
   );
@@ -48,14 +50,23 @@ function Nav(){
     </div>
   );
 }
-function Modal(){
+function Modal(props){
   return(
     <div className='modal'>
-      <h4>제목</h4>
+      <h4>{props.articleTitle[0]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={()=>{
+        props.setArticleTitle(editTitle(props.articleTitle));
+      }}>글 수정</button>
     </div>
   );
+}
+
+function editTitle(articleTitle){
+  let copy = [...articleTitle];
+  copy[0] = '블로그 고인물';
+  return copy;
 }
 
 
