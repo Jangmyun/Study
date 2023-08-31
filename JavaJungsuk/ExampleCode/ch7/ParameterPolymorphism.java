fd93a617f6558ac3e7533940de6962d06a262cbb
Jangmyun <cndlsrb2739@gmail.com>
2023-08-31 20:47:20 +0900

    Buyer 클래스 추가 후 구입 메서드와 잔액확인 메서드 추가

--- JavaJungsuk/ExampleCode/ch7/ParameterPolymorphism.java
+++ JavaJungsuk/ExampleCode/ch7/ParameterPolymorphism.java
@@ -12,7 +12,7 @@ class Product {
     }
 }
 class Tv1 extends Product {
-    Tv1(){                      //조상 클래스의 생성자 호출
+    Tv1(){                      //조상 클래스의 생성자 호출 (price 멤버변수는 조상한테서 상속된 것 이므로 조상클래스의 생성자 이용)
         super(100);
     }
 
@@ -21,7 +21,7 @@ class Tv1 extends Product {
     }
 }
 class Computer extends Product {
-    Computer(){                 //조상 클래스의 생성자 호출 (price 멤버변수는 조상한테서 상속된거라서)
+    Computer(){                 //조상 클래스의 생성자 호출 
         super(200);
     }
     public String toString(){
@@ -29,8 +29,32 @@ class Computer extends Product {
     }
 }
 
-public class ParameterPolymorphism {
-    public static void main(String[] args) {
-        System.out.println();
+class Buyer {
+    int money = 1000;
+    int bonusPoint=0;
+
+    void buy(Product p){        //다형성을 이용해서 Product 클래스를 상속받은 다른 클래스도 들어갈 수 있음
+        if( this.money< p.price ){
+            System.out.println("잔액부족");
+            return;
+        }
+        this.money -= p.price;
+        this.bonusPoint += p.bonusPoint;
+        System.out.println(p + " 구입완료");
+    }
+    void checkMoney(){
+        System.out.println("현재잔액: "+this.money);
     }
 }
+
+public class ParameterPolymorphism {
+    public static void main(String[] args) {
+        Buyer buyer = new Buyer();
+
+        buyer.buy(new Computer());
+        buyer.checkMoney();
+        buyer.buy(new Tv1());
+        buyer.checkMoney();
+    }
+}
+
