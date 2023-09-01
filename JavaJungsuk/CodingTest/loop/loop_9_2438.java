29165e57da68ccd8d2aebde12bb857503640fa68
Jangmyun <cndlsrb2739@gmail.com>
2023-09-01 23:20:45 +0900

    별찍기 문제

---
+++ JavaJungsuk/CodingTest/loop/loop_9_2438.java
@@ -0,0 +1,16 @@
+package CodingTest.loop;
+import java.util.Scanner;
+public class loop_9_2438 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
