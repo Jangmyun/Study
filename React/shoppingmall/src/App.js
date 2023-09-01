9e4f2350cfbf9528207e85b11827a1bdc6bb2e04
Jangmyun <cndlsrb2739@gmail.com>
2023-09-01 11:17:11 +0900

    Navbar Component 작성

--- React/shoppingmall/src/App.js
+++ React/shoppingmall/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import './Navbar.css'
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar></Navbar>
         
+    </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-logo">
+        <h4>Jangmyun.shop</h4>
+      </div>
+      <div className="navbar-nav">
+        <a href="#home">Home</a>
+        <a href="#cart">Cart</a>
+      </div>
     </div>
   );
 }
