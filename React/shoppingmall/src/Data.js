ac541afbff2568ef9a435351d259ea1cae8f9466
Jangmyun <cndlsrb2739@gmail.com>
2023-09-02 18:12:00 +0900

    App.js로 import할 data export하기

--- React/shoppingmall/src/Data.js
+++ React/shoppingmall/src/Data.js
@@ -1,22 +1,26 @@
+import {useState} from 'react';
+
 let data = [
     {
       id : 0,
-      title : "White and Black",
-      content : "Born in France",
-      price : 120000
+      title : "Airforce",
+      content : "The radiance lives on with the b-ball original. Crossing hardwood comfort with off-court flair, it puts a fresh spin on what you know best: ‘80s-inspired construction, bold details and nothin’-but-net style.",
+      price : 160000
     },
   
     {
       id : 1,
-      title : "Red Knit",
-      content : "Born in Seoul",
-      price : 110000
+      title : "Basketball Shoes",
+      content : "Dynamic Responsiveness, Advanced Traction, On-Court Comfort",
+      price : 130000
     },
   
     {
       id : 2,
-      title : "Grey Yordan",
-      content : "Born in the States",
-      price : 130000
+      title : "Running Shoes",
+      content : "This version has the same responsiveness and neutral support you love, but with improved comfort in those sensitive areas of your foot, like the arch and toes.",
+      price : 140000
     }
   ] ;
+
+  export default data;
