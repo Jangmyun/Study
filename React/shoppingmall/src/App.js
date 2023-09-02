a9b55495211688a77cdc4fe56a0c8edb53938e40
Jangmyun <cndlsrb2739@gmail.com>
2023-09-02 21:24:18 +0900

    Routes와 Route import한 후 페이지 라우팅

--- React/shoppingmall/src/App.js
+++ React/shoppingmall/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import './Navbar.css';
 import './Product.css';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
+import {Row, Col, Container} from 'react-bootstrap';
 import data from './Data';
-import { useState } from 'react';
+import {useState} from 'react';
+import {Routes, Route, Link} from 'react-router-dom';
 
 
 
@@ -16,20 +15,14 @@ function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+
       <div className="main-bg"></div>
-      <Container className='productBox'>
-        <Row>
-          { // Data.js에서 import 한 
-            shoes.map(function(value, index){
-              return(
-                <ProductCard  src={"https://jangmyun.github.io/img/React/Product/product"+ (index+1) +".jpg"}
-                            title={value.title} content={value.content} price={value.price}
-                />
-              );
-            })
-          }
-        </Row>
-      </Container>
+
+      <Routes>
+        <Route path='/' element={<ProductList />}></Route>
+        <Route path='/cart' element={<div>Cart</div>}></Route>
+      </Routes>
+      
     </div>
   );
 }
@@ -41,10 +34,28 @@ function Navbar() {
         <h4>Jangmyun.shop</h4>
       </div>
       <div className="navbar-nav">
-        <a className="active" href="#home">Home</a>
-        <a href="#cart">Cart</a>
+        <a className="active" href="/">Home</a>
+        <a href="/cart">Cart</a>
       </div>
     </div>
+  );
+}
+function ProductList(){
+  let [shoes, setShoes] = useState(data);
+  return (
+    <Container className='productBox'>
+        <Row>
+          { // Data.js에서 import 한 data 반복문 돌려서 ProductCard 생성
+            shoes.map(function(value, index){
+              return(
+                <ProductCard  src={"https://jangmyun.github.io/img/React/Product/product"+ (index+1) +".jpg"}
+                            title={value.title} content={value.content} price={value.price}
+                />
+              );
+            })
+          }
+        </Row>
+      </Container>
   );
 }
 
