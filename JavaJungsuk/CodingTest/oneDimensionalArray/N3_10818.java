745dac98a1e533e9568f715fb0ee72ca5a869cf1
Jangmyun <cndlsrb2739@gmail.com>
2023-09-03 11:02:32 +0900

    2562번 문제 풀이로 코드 수정

--- JavaJungsuk/CodingTest/oneDimensionalArray/N3_10818.java
+++ JavaJungsuk/CodingTest/oneDimensionalArray/N3_10818.java
@@ -1,35 +1,28 @@
 package CodingTest.oneDimensionalArray;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class N3_10818 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-
-        String tmp = scanner.nextLine();
-        int N = Integer.parseInt(tmp);
-        int[] nArr = new int[N];
-        tmp = scanner.nextLine();
-        StringTokenizer st = new StringTokenizer(tmp);
-
-        int i=0;
-        Integer min = null;
+        int[] numArr = new int[9];
         Integer max = null;
-        while( st.hasMoreTokens()){
-            nArr[i] = Integer.parseInt(st.nextToken());
-            if( min == null || max == null) {
-                min = nArr[i];
-                max = nArr[i];
-            }
+        int index=0;
 
-            if(min >nArr[i]){
-                min = nArr[i];
+        for( int i=0; i<numArr.length; i++){
+            numArr[i] = scanner.nextInt();
+            if( max == null) {
+                max = numArr[i];
             }
-            if(max<nArr[i]){
-                max = nArr[i];
+            if( max < numArr[i]){
+                max = numArr[i];
+                index = i;
             }
         }
-        System.out.printf("%d %d", min, max);
+        System.out.printf("%d%n%d",max, index);
+
+
+        
+        
+        scanner.close();
     }
 }
