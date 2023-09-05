e34129a0ad8fba285dea33d4e9e440c9fef2f989
Jangmyun <cndlsrb2739@gmail.com>
2023-09-05 17:44:24 +0900

    substring 메서드를 사용한 방법 주석으로 추가

--- JavaJungsuk/CodingTest/String/N5_11720.java
+++ JavaJungsuk/CodingTest/String/N5_11720.java
@@ -16,3 +16,35 @@ public class N5_11720 {
         System.out.println(sum);
     }
 }
+
+/*
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st;
+		
+		int len = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine());
+		
+		String str = st.nextToken();
+		
+		int sum = 0;
+		
+		for(int i=0; i<len; i++) {
+			sum += Integer.parseInt(str.substring(i,i+1));
+		}
+		
+		System.out.println(sum);
+		
+	}
+
+}
+
+ */
