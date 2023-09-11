9397421eb14aae34553b66f8de18d9fb959b93d9
Jangmyun <cndlsrb2739@gmail.com>
2023-09-11 12:11:57 +0900

    GET 요청으로 HTML 파일 받아오기

--- NodeJs/firstNodeJs/index.js
+++ NodeJs/firstNodeJs/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.listen(6000, function(){
-    console.log('listening on 6000');
+app.listen(8080, function(){
+    console.log('listening on 8080');
 });
+
+app.get('/car', function(request, respond){
+    respond.send('자동차 쇼핑몰임.');
+})
+
+app.get('/computer', function(request, respond){
+    respond.send('컴퓨터 쇼핑몰임.');
+})
+
+app.get('/', function(request, respond){
+    respond.sendFile(__dirname + '/client/index.html');
+})
