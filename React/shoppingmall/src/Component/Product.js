2ff03eeced261114822b3e9f56d1b2025894825d
Jangmyun <cndlsrb2739@gmail.com>
2023-09-11 18:47:49 +0900

    새 Product 불러오기 기능 완성

--- React/shoppingmall/src/Component/Product.js
+++ React/shoppingmall/src/Component/Product.js
@@ -6,32 +6,36 @@ import '../css/App.css';
 import styled from 'styled-components';
 import axios from 'axios';
 
-function ProductList({productId, shoes}){
+function ProductList({productId, shoes, setShoes}){
   return (
     <Container className='productBox'>
         <Row>
           { // Data.js에서 import 한 data 반복문 돌려서 ProductCard 생성
             shoes.map(function(value, index){
               return(
-                <ProductCard id={index} src={"https://jangmyun.github.io/img/React/Product/product"+ (index+1) +".jpg"}
-                            title={value.title} content={value.content} price={value.price} productId={productId}
+                <ProductCard id={value.id} src={"https://jangmyun.github.io/img/React/Product/product"+ (index+1) +".jpg"}
+                title={value.title} content={value.content} price={value.price} key={value.id}
                 />
               );
             })
           }
-        </Row>
-        <button onClick={()=>{
+          <button onClick={()=>{
           axios.get('https://jangmyun.github.io/data/json/productData2.json')
-          .then((data)=>{console.log(data)});
+          .then((result)=>{
+            let copy = [...shoes, ...result.data];
+            setShoes(copy);
+          });
         }}>불러오기</button>
+        </Row>
+        
       </Container>
   );
 }
 
-function ProductCard({id, src, title, content, price, key}){
+function ProductCard({id, src, title, content, price}){
   return (
     <Col  xs={6} md={4} lg={4} onClick={()=>{
-      window.location.href = "/detail/"+ (id);
+      window.location.href = "/detail/"+ (id); 
     }}>
       <img width={'100%'} src={src} alt="" />
       <h4>{title}</h4>
