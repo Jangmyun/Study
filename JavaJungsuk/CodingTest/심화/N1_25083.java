536459f61b21e3069320e2e07a13c94037930e78
Jangmyun <cndlsrb2739@gmail.com>
2023-09-12 11:17:02 +0900

    심화 코딩테스트 문제 풀이

---
+++ JavaJungsuk/CodingTest/심화/N1_25083.java
@@ -0,0 +1,12 @@
+package CodingTest.심화;
+
+public class N1_25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r\'\"7");
+        System.out.println("r`-_   ,\'  ,/");
+        System.out.println(" \\. \". L_r\'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
