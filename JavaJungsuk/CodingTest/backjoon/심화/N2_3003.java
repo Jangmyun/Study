98b1fd441134e7f5f2729aa50a59c66aece574a0
Jangmyun <cndlsrb2739@gmail.com>
2023-09-13 16:04:24 +0900

    디렉토리 이동

---
+++ JavaJungsuk/CodingTest/backjoon/심화/N2_3003.java
@@ -0,0 +1,18 @@
+package CodingTest.backjoon.심화;
+import java.io.*;
+import java.util.StringTokenizer;
+public class N2_3003 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] chessTemplate = {1, 1, 2, 2, 2, 8};
+        int[] dongHyukPieces = new int[chessTemplate.length];
+        String pieceString = br.readLine();
+        StringTokenizer st = new StringTokenizer(pieceString);
+        
+        for(int i=0; i<chessTemplate.length; i++ ){
+            int a = Integer.parseInt(st.nextToken());
+            dongHyukPieces[i] = chessTemplate[i] - a;
+            System.out.printf("%d ",dongHyukPieces[i]);
+        }
+    }
+}
