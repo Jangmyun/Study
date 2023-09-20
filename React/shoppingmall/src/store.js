838eca471f6da8d0292dfc4a7a011421a38a3da2
Jangmyun <cndlsrb2739@gmail.com>
2023-09-20 21:11:35 +0900

    cart state 변경 함수

--- React/shoppingmall/src/store.js
+++ React/shoppingmall/src/store.js
@@ -2,11 +2,31 @@ import {configureStore , createSlice} from '@reduxjs/toolkit';
 
 let user = createSlice({
     name: 'user',
-    initialState: 'kim'
+    initialState: {name:'kim', age: 21},
+    reducers: {
+        setUser(user){
+            user.name = 'park';
+            user.age = user.age+1;
+        }
+    }
 })
+export let {setUser} = user.actions;
 
+let cart = createSlice({
+    name: 'cart',
+    initialState:[
+        {id : 0, name : 'White and Black', count : 2},
+        {id : 2, name : 'Grey Yordan', count : 1}
+      ] ,
+    reducers: {
+        setCart(cart, id){
+            cart.findIndex( obj => obj.id == id)
+        }
+    }
+})
 export default configureStore({
     reducer: {
-        user: user.reducer
+        user: user.reducer,
+        cart: cart.reducer
     }
 })
