0bb16d770dfdfd517e5a8c562e9ea07b2b3e1a41
Jangmyun <cndlsrb2739@gmail.com>
2023-09-27 11:11:53 +0900

    index 파일의 기본 타입 정리

---
+++ TypeScript/ts1/primitiveTypes.ts
@@ -0,0 +1,10 @@
+let myName = 'Chu';
+let myAge = 22;
+
+let favorite :{title: string, singer:string} = {title: 'd', singer:'kim'};
+
+let project :{ member:string[], days:number, started: boolean} = {
+    member:['kim', 'park'],
+    days: 30,
+    started: false
+}
