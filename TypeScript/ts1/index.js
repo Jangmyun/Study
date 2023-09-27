76bafd63e98c7ae2b606a7d42b15cc5809664fe9
Jangmyun <cndlsrb2739@gmail.com>
2023-09-27 11:12:26 +0900

    tsc

--- TypeScript/ts1/index.js
+++
@@ -1,8 +0,0 @@
-var nam = 'kim';
-var john = { name: 'kim', age: '123' };
-var User = /** @class */ (function () {
-    function User(name) {
-        this.name = name;
-    }
-    return User;
-}());
---
+++ TypeScript/ts1/primitiveTypes.js
@@ -0,0 +1,8 @@
+var myName = 'Chu';
+var myAge = 22;
+var favorite = { title: 'd', singer: 'kim' };
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: false
+};
