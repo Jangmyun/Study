efb8db837845ebe600b8ff0fd7bb7f0c3b08d512
Jangmyun <cndlsrb2739@gmail.com>
2023-10-06 10:10:18 +0900

    react redux 사용방법 공부

---
+++ React/react-redux/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { useState } from "react"; //useState 사용할때 import 해야함
+
+function App() {
+  return(
+    <div className='app'></div>
+  );
+}  
+
+
+
+
+export default App;
