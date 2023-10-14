d40ed4ac9c6f36591e31c33993a798a51d7d6e25
Jangmyun <cndlsrb2739@gmail.com>
2023-10-14 17:36:09 +0900

    N킬로그램의 설탕을 배달할 때 필요한 봉지의 최솟값 구하는 문제

---
+++ algorithm/greedy/SugarDelivery.java
@@ -0,0 +1,59 @@
+package algorithm.greedy;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class SugarDelivery {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    minimumBag(N);
+  }
+
+  private static void minimumBag(int N) {
+    ArrayList<Integer> resultArrayList = new ArrayList<>();
+    ArrayList<Integer> checkResult = new ArrayList<>();
+    int result = 0;
+
+    for (int i = 0; N%5 <= N - (i * 5); i++) {
+      result = 0;
+
+      int leftSugar = N - (i * 5);
+      result += i;
+
+      if( leftSugar == 0){
+        resultArrayList.add(result);
+      }
+      else {
+        result += leftSugar/3;
+        leftSugar = leftSugar%3;
+
+        if(leftSugar != 0) {
+          resultArrayList.add(-1);
+        }
+        else {
+          resultArrayList.add(result);
+        }
+      }
+    } // end of i
+
+    for (int resultValue : resultArrayList) {
+      if (!checkResult.contains(resultValue)) {
+        checkResult.add(resultValue);
+      }
+    }
+
+    int min = 10000;
+    for (int resultValue : checkResult) {
+      if (resultValue != -1 && resultValue < min) {
+        min = resultValue;
+      }
+    }
+    if (min == 10000) {
+      System.out.println(-1);
+      return;
+    }
+    System.out.println(min);
+
+  }
+}
