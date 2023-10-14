3c54a3e4d814b20de70e51726a4c17e2a71af419
Jangmyun <cndlsrb2739@gmail.com>
2023-10-14 18:41:08 +0900

    설탕 배달 문제를 더 간결한 코드로 재작성

---
+++ algorithm/greedy/SugarDelivery2.java
@@ -0,0 +1,32 @@
+package algorithm.greedy;
+
+import java.io.*;
+
+public class SugarDelivery2 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int result = 0;
+
+    if (n % 5 == 0) {
+      System.out.println(n/5);
+    } else {
+      
+      while ( n>0){
+        if(n%5 == 0){
+          result += n/5;
+          System.out.println(result);
+          return;
+        }
+        if(n<3){
+          System.out.println(-1);
+          return;
+        }
+        n-=3;
+        result++;
+      }
+      System.out.println(result);
+    }
+  }
+}
+
