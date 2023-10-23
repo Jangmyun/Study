273bbd65704921256704d4b2f54749dd52748da4
Jangmyun <cndlsrb2739@gmail.com>
2023-10-23 16:40:59 +0900

    회의실 문제 다시 풀기

--- algorithm/greedy/MeetingRoom.java
+++ algorithm/greedy/MeetingRoom.java
@@ -4,33 +4,21 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class MeetingRoom {
-  public static void main(String[] args) throws IOException{
+  public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
-    Meeting[] meetingArr = new Meeting[n];
-    for(int i=0; i<meetingArr.length; i++){
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      meetingArr[i] = new Meeting(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+
+    int[][] time = new int[n][2];
+    StringTokenizer st;
+
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
+      time[i][0] = Integer.parseInt(st.nextToken());
+      time[i][1] = Integer.parseInt(st.nextToken());
     }
+    
+
 
   }
-  
-}
-class Meeting {
-  int start;
-  int end;
-  Meeting(int start, int end){
-    this.start = start;
-    this.end = end;
-  }
-  @Override
-  public String toString(){
-    return "start: "+ this.start + " // end: "+this.end;
-  }
 
-  static void printMeetingArr(Meeting[] meetings){
-    for(Meeting meeting : meetings){
-      System.out.println(meeting.toString());
-    }
-  }
 }
