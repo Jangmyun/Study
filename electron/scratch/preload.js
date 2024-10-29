75c371ade15c7582c0f9b25176d737cb27536850
Jangmyun <cndlsrb2739@gmail.com>
2024-10-29 10:46:54 +0900

    preload add ping invoke

--- electron/scratch/preload.js
+++ electron/scratch/preload.js
@@ -1,8 +1,9 @@
-const { contextBridge } = require("electron");
+const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("versions", {
   node: () => process.version.node,
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke("ping"),
   // 함수 말고도 var expose 가능
 });
